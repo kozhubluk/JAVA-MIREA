41a2625c93b0888dc9e0642384f50fc9b52a92d6
kozhubluk <nastya.awdeeva@yandex.ru>
2022-12-07 18:44:49 +0300

    29

---
+++ java_3sem/src/Practice29/Item.java
@@ -0,0 +1,7 @@
+package Practice29;
+
+public interface Item {
+    int getCost();
+    String getName();
+    String getDescription();
+}
---
+++ java_3sem/src/Practice29/OrderAlreadyAddedException.java
@@ -0,0 +1,13 @@
+package Practice29;
+
+public class OrderAlreadyAddedException extends Exception{
+    private String address;
+    public OrderAlreadyAddedException(String add) {
+        super("Order already added");
+        address = add;
+    }
+
+    public String getMessage() {
+        return super.getMessage() + ": " + address;
+    }
+}
